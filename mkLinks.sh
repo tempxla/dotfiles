cdbef455881497a468c7070cf9581d97c147c624
tempxla <38803749+tempxla@users.noreply.github.com>
2018-04-29 23:36:40 +0900

    add config

--- mkLinks.sh
+++ mkLinks.sh
@@ -2,3 +2,5 @@
 THIS_DIR=$(cd $(dirname $0); pwd)
 ln -sf ${THIS_DIR}/.xmonad/xmonad.hs    ~/.xmonad/xmonad.hs
 ln -sf ${THIS_DIR}/.xmobarrc            ~/.xmobarrc
+ln -sf ${THIS_DIR}/.Xresources          ~/.Xresources
+ln -sf ${THIS_DIR}/.config/fontconfig/fonts.conf    ~/.config/fontconfig/fonts.conf
