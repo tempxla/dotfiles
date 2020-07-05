f87e22fcf9180df6baecfcbdc027387d7746882b
tempxla <38803749+tempxla@users.noreply.github.com>
2020-07-05 21:30:13 +0900

    fish

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -41,7 +41,7 @@ if not set -q __fish_prompt_cwd
             set -g __fish_prompt_cwd (set_color $fish_color_cwd)
         end
 
-printf '[%s] %s%s@%s %s%s %s(%s)%s \f\r%s> ' (date "+%H:%M:%S") "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal" (__fish_git_prompt)
+printf '[%s%s@%s] %s%s %s(%s)%s \f\r%s> ' "$__fish_color_blue" $USER $__fish_prompt_hostname "$__fish_prompt_cwd" "$PWD" "$__fish_color_status" "$stat" "$__fish_prompt_normal" (__fish_git_prompt)
 
 end
 end
--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -5,4 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (cider undohist undo-tree sml-modeline intero go-eldoc flymake-haskell-multi company-go company-ghc auto-highlight-symbol))))
+    (fish-mode cider undohist undo-tree sml-modeline intero go-eldoc flymake-haskell-multi company-go company-ghc auto-highlight-symbol))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
