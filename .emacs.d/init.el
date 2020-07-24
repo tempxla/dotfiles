17228ecfa757317c23fbc4f09042a92142280d2f
tempxla <38803749+tempxla@users.noreply.github.com>
2020-07-24 23:11:47 +0900

    emacs color

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,6 +119,7 @@
 (set-face-foreground 'font-lock-constant-face      "cyan"   )
 ;; (set-face-foreground 'font-lock-operator-face      "yellow"   ) ; 演算子は無い
 (set-face-background 'region                       "#444444")
+(set-face-foreground 'vertical-border              "gray30" )
 
 ;; -----------------------------------------------------------------------------
 ;; 見た目
