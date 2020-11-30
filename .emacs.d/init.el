124e0ca89b04347c5b5a91926798697ea3c7f551
tempxla <38803749+tempxla@users.noreply.github.com>
2020-11-30 21:31:28 +0900

    dired: a-key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -439,6 +439,8 @@
 
 (setq diredp-hide-details-initially-flag nil)
 (setq dired-details-propagate-flag t)
+
+(put 'dired-find-alternate-file 'disabled nil)
 ;;(define-key dired-mode-map (kbd "q")         '(lambda () (interactive) (quit-window t)))
 (define-key dired-mode-map (kbd "M-RET")     'dired-do-async-shell-command)
 (when (require 'dired+ nil t)
