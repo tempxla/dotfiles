147a597e3ffe803713f1293495e982a21a293b27
tempxla <38803749+tempxla@users.noreply.github.com>
2021-01-11 23:59:04 +0900

    run-mpd: add else

--- bin/run-mpd.sh
+++ bin/run-mpd.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-if [ ! -s ~/.config/mpd/pid ] ; then
+pid_file=~/.config/mpd/pid
+
+if [ ! -s $pid_file ] ; then
     # for io_uring, changed ulimit -l
     # https://github.com/MusicPlayerDaemon/MPD/issues/972
     # /etc/security/limits.conf
     # @wheel           hard    memlock         67108864
     ulimit -l 67108864
     mpd ~/.config/mpd/mpd.conf
+else
+    echo "[WARN] mpd didn't start: $pid_file exists."
 fi
