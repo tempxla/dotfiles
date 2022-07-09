9b9f3a5e88d7448f1d1fb9519cc370891ae359f5
tempxla <38803749+tempxla@users.noreply.github.com>
2022-07-09 11:59:00 +0900

    add set-volume-default.sh

--- mkLinks.sh
+++ mkLinks.sh
@@ -3,15 +3,16 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 HOSTNAME=$(hostname)
 
 ## WM & DE
-ln -sf ${THIS_DIR}/.xmonad/xmonad.hs      ~/.xmonad/xmonad.hs
-ln -sf ${THIS_DIR}/.xmobarrc              ~/.xmobarrc
-ln -sf ${THIS_DIR}/bin/run-urxvtd.sh      ~/bin/run-urxvtd.sh
-ln -sf ${THIS_DIR}/bin/get-volume.sh      ~/bin/get-volume.sh
-ln -sf ${THIS_DIR}/bin/get-os-release.sh  ~/bin/get-os-release.sh
-ln -sf ${THIS_DIR}/.Xresources            ~/.Xresources
-ln -sf ${THIS_DIR}/.Xmodmap               ~/.Xmodmap
+ln -sf ${THIS_DIR}/.xmonad/xmonad.hs         ~/.xmonad/xmonad.hs
+ln -sf ${THIS_DIR}/.xmobarrc                 ~/.xmobarrc
+ln -sf ${THIS_DIR}/bin/run-urxvtd.sh         ~/bin/run-urxvtd.sh
+ln -sf ${THIS_DIR}/bin/get-volume.sh         ~/bin/get-volume.sh
+ln -sf ${THIS_DIR}/bin/set-volume-default.sh ~/bin/set-volume-default.sh
+ln -sf ${THIS_DIR}/bin/get-os-release.sh     ~/bin/get-os-release.sh
+ln -sf ${THIS_DIR}/.Xresources               ~/.Xresources
+ln -sf ${THIS_DIR}/.Xmodmap                  ~/.Xmodmap
 ln -sf ${THIS_DIR}/.config/fontconfig/fonts.conf  ~/.config/fontconfig/fonts.conf
-ln -sf ${THIS_DIR}/bin/run-xrandr.sh      ~/bin/run-xrandr.sh
+ln -sf ${THIS_DIR}/bin/run-xrandr.sh         ~/bin/run-xrandr.sh
 
 ## shell
 ln -sf ${THIS_DIR}/.inputrc             ~/.inputrc
